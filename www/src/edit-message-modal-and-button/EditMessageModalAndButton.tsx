4b4479a5e6f8454c4988439a0482425251c69fce
Henry Zimmerman <zimhen7@gmail.com>
2017-10-14 16:37:23 -0400

    full editing support

--- www/src/edit-message-modal-and-button/EditMessageModalAndButton.tsx
+++ www/src/edit-message-modal-and-button/EditMessageModalAndButton.tsx
@@ -14,7 +14,8 @@ import App from "../App";
 
 export interface EditMessageModalAndButtonState {
   modal: boolean;
-  message: AppUserMessage;
+  text: string;
+  uuid: string;
 }
 
 export interface EditMessageModalAndButtonProps {
@@ -34,10 +35,14 @@ export class EditMessageModalAndButton extends React.Component<EditMessageModalA
     super(props);
     this.state = {
       modal: false,
-      message: props.message
+      text: props.message.text,
+      uuid: props.message.uuid
     };
 
     this.toggle = this.toggle.bind(this);
+    this.editMessage = this.editMessage.bind(this);
+    this.editAndClose = this.editAndClose.bind(this);
+    this.handleChange = this.handleChange.bind(this);
   }
 
 
@@ -50,8 +55,6 @@ export class EditMessageModalAndButton extends React.Component<EditMessageModalA
   }
 
 
-
-
   toggle() {
     this.setState({
       modal: !this.state.modal
@@ -60,11 +63,10 @@ export class EditMessageModalAndButton extends React.Component<EditMessageModalA
 
   editMessage() {
     let toBeEdited: AppUserMessage = new AppUserMessage();
-    toBeEdited.text = "I like Apples";
-    toBeEdited.uuid = this.state.message.uuid;
+    toBeEdited.text = this.state.text;
+    toBeEdited.uuid = this.state.uuid;
 
     this.context.store.dispatch(
-
       {
         type: MessageStoreActionEnum.EDIT_MESSAGE,
         message: toBeEdited
@@ -72,25 +74,35 @@ export class EditMessageModalAndButton extends React.Component<EditMessageModalA
     );
   }
 
+  editAndClose() {
+    this.editMessage();
+    this.toggle();
+  }
+
+  handleChange(event: React.SyntheticEvent<HTMLTextAreaElement>) {
+    this.setState({text: event.currentTarget.value});
+  }
+
+
   render() {
     return (
       <div>
-        <div className={"Icon"} onClick={this.toggle}  id={"EditMessageButton" + this.state.message.uuid}>
+        <div className={"Icon"} onClick={this.toggle}  id={"EditMessageButton" + this.state.uuid}>
           <FontAwesome name="pencil"/>
         </div>
-        <UncontrolledTooltip placement="left" target={"EditMessageButton" + this.state.message.uuid}>
+        <UncontrolledTooltip placement="left" target={"EditMessageButton" + this.state.uuid}>
           Edit Message
         </UncontrolledTooltip>
 
         <Modal isOpen={this.state.modal} toggle={this.toggle} >
           <ModalHeader toggle={this.toggle}>Edit Message</ModalHeader>
           <ModalBody>
-            <textarea rows={5} style={{width: "100%", backgroundColor: "gainsboro", color: "#242424"}} >
-            {this.state.message.text}
+            <textarea rows={5} style={{width: "100%", backgroundColor: "gainsboro", color: "#242424"}} value={this.state.text} onChange={this.handleChange} >
+            {/*{this.state.message.text}*/}
             </textarea>
           </ModalBody>
           <ModalFooter>
-            <Button onClick={this.editMessage} color={"success"}>Save</Button> // TODO use a method to set this message's content higher up in the DOM tree.
+            <Button onClick={this.editAndClose} color={"success"}>Save</Button>
             <Button onClick={this.toggle} color={"danger"}>Cancel</Button>
           </ModalFooter>
         </Modal>
