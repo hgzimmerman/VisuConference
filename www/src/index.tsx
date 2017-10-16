d76da829205771f427a12e71907fc95d496c1343
Henry Zimmerman <zimhen7@gmail.com>
2017-10-16 12:43:07 -0400

    cleanup imports

--- www/src/index.tsx
+++ www/src/index.tsx
@@ -1,6 +1,5 @@
 // import "jquery/src/jquery";
 import "jquery";
-// import "popper.js";
 import "popper.js";
 import * as React from "react";
 import * as ReactDOM from "react-dom";
@@ -9,7 +8,6 @@ import registerServiceWorker from "./registerServiceWorker";
 import "./index.css";
 // To import bootstrap here, you need tether, but tether can't be imported without webpack, which this project doesn't use.
 // So in public/index.html, there is a script to get the tether source. That will run before anything here, allowing bootstrap to work.
-require("popper.js");
 import "bootstrap";
 import "bootstrap/dist/css/bootstrap.css";
 import "../node_modules/font-awesome/css/font-awesome.min.css";
