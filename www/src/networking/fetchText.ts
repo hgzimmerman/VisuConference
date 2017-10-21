c88372eac39b6875f725e95f9ad07e34a3a2a8dd
Henry Zimmerman <zimhen7@gmail.com>
2017-10-21 08:54:37 -0400

    added env vars

--- www/src/networking/fetchText.ts
+++ www/src/networking/fetchText.ts
@@ -5,7 +5,10 @@ import { AppUserMessage, IncomingUserMessage} from "../datatypes/message";
 export namespace Networking {
 
   export async function fetchTrumpText(): Promise<AppUserMessage> {
-    const response = await fetch("http://localhost:8000/trump/4");
+    let requestUrl = `http://${process.env.REACT_APP_ENDPOINT_URL}:8000/trump/4`;
+    console.log(requestUrl);
+
+    const response = await fetch(requestUrl);
     const incomingMessage: IncomingUserMessage = await response.json();
     return await AppUserMessage.fromIncomingUserMessage(incomingMessage);
   }
