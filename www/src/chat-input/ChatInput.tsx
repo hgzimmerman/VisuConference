b22d87fc585634cd1196cc559a407077e540c424
Henry Zimmerman <zimhen7@gmail.com>
2017-11-02 17:59:38 -0400

    Fixed Recording button, added a disabled tts button.

--- www/src/chat-input/ChatInput.tsx
+++ www/src/chat-input/ChatInput.tsx
@@ -13,15 +13,22 @@ export class ChatInput extends React.Component {
   };
   context: any;
 
-  public state: {value: string};
+  public state: {
+    value: string
+    isRecording: boolean
+  };
   private unsubscribe: Function;
 
   constructor(props: {}) {
     super(props);
 
-    this.state = {value: ""};
+    this.state = {
+      value: "",
+      isRecording: true
+    };
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleToggleRecording = this.handleToggleRecording.bind(this);
   }
 
   componentDidMount() {
@@ -56,22 +63,49 @@ export class ChatInput extends React.Component {
     }
   }
 
+  handleToggleRecording() {
+    this.setState( {
+        value: this.state.value,
+        isRecording: !this.state.isRecording
+      }
+    );
+  }
+
   render() {
+
+    let recordingEntry;
+    if (this.state.isRecording) {
+      recordingEntry = (
+        <div className={"dropdown-item"} onClick={this.handleToggleRecording}>
+          Recording
+          <FontAwesome className="Icon" style={{color: "red"}} name="dot-circle-o"/>
+      </div>
+      );
+    } else {
+      recordingEntry = (
+        <div className={"dropdown-item"} onClick={this.handleToggleRecording}>
+          Not Recording
+          <FontAwesome className="Icon" style={{color: "gray"}} name="dot-circle-o"/>
+      </div>
+      );
+    }
+
     return (
       <div className="ChatInput">
         <form onSubmit={this.handleSubmit} className="d-flex">
           <input className="Input form-control" type="text" placeholder="Chat" value={this.state.value} onChange={this.handleChange} />
 
           <div className="btn-group dropup" >
-            <button type="button" className="btn btn-light dropdown-toggle dropdown-toggle-split" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            <button type="button" className="btn btn-light dropdown-toggle-split" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
               <span className="sr-only">Toggle Dropdown</span>
+              <FontAwesome name="cog"/>
             </button>
             <div className="dropdown-menu">
-              <div>
-                <FontAwesome style={{color: "red"}} name="dot-circle-o"/>
-                Currently Recording
+              {recordingEntry}
+              <div className={"dropdown-item"}>
+                Text To Speech Off
+                <FontAwesome className="Icon" style={{color: "gray"}} name="volume-off"/>
               </div>
-              {/*{JSON.stringify(this.context.store) + "hello"}*/}
             </div>
             <button type="submit" className="btn btn-light SendButtons">Send</button>
           </div>
